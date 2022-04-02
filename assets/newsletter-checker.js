47d8f40b1a1966ba502f83294136f4165e1b1e9f
chalalala <neko_hime1212@yahoo.com>
2022-04-03 00:56:36 +0700

    fix: update layout and form checker

--- assets/newsletter-checker.js
+++ assets/newsletter-checker.js
@@ -1,14 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const newsletterInput = document.querySelector(".js-newsletter-input");
+	const tooltip = document.querySelector(".js-newsletter-tooltip");
 
-	if (newsletterInput) {
+	if (newsletterInput && tooltip) {
 		const newsletterInputContainer = newsletterInput.parentElement;
-
 		newsletterInput.addEventListener("blur", () => {
-			if (!newsletterInput.value) {
+			const value = newsletterInput.value;
+			const validMail = value.match(/.*@.*\..*/);
+
+			if (!value) {
 				newsletterInputContainer.classList.add("error");
+				tooltip.textContent = "This field is required";
+			} else if (!validMail) {
+				newsletterInputContainer.classList.add("error");
+				tooltip.textContent = "Please enter a valid email address";
 			} else {
 				newsletterInputContainer.classList.remove("error");
+				tooltip.textContent = "";
 			}
 		});
 	}
