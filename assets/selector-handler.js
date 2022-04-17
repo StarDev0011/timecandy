7d0ff971bab1ed40a2a55e6fa7752dfd9b958603
chalalala <neko_hime1212@yahoo.com>
2022-04-17 21:43:03 +0700

    feat: update script of selector

--- assets/selector-handler.js
+++ assets/selector-handler.js
@@ -4,13 +4,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	if (selector) {
 		const options = selector.options;
 
-		for (option of options) {
-			if (window.location.pathname === option.getAttribute("target")) {
-				selector.value = option.value;
-				break;
-			}
-		}
-
 		selector.addEventListener("change", () => {
 			let redirectLink = options[selector.selectedIndex].getAttribute("target");
 			window.location.href = redirectLink;
