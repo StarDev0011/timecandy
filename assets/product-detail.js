99597f7f4cbe5cdf8a04ace62ab3489bdc53a6db
hamsa <dev@hamsa.co>
2022-04-28 08:26:15 +0700

    collection page

--- assets/product-detail.js
+++ assets/product-detail.js
@@ -5,6 +5,8 @@ Product = {
     swatchName: '.js-option-label',
     swatchInput: '.js-variant-input',
     swatchLabel: '.product-swatch__label',
+    swatchImage: '.js-swatch-image',
+    swatchSoldOut: '.js-option-sold-out',
     btnReadMore: '.js-read-more'
   },
 
@@ -17,6 +19,16 @@ Product = {
     const variantTile = $(`${ Product.Class.swatchInput }:checked`).val();
     $(Product.Class.swatchName).text(variantTile);
 
+    if($(Product.Class.swatchImage).length) {
+      const varinatImg = $(`${ Product.Class.swatchInput }:checked`).next().find('.product-form__swatch-image').attr('src');
+      $(Product.Class.swatchImage).attr('src', varinatImg).show();
+    }
+
+    if($(Product.Class.swatchSoldOut).length) {
+      const soldOut = $(`${ Product.Class.swatchInput }:checked`).next().find('.product-form__swatch-soldout').text();
+      $(Product.Class.swatchSoldOut).text(soldOut);
+    }
+
     $('body').on('click', Product.Class.swatchLabel, function() {
       $(this).next().slideToggle(200);
     });
@@ -25,6 +37,13 @@ Product = {
       const name = $(this).val();
       $(Product.Class.swatchName).text(name);
       $('.product-form__swatch-dropdown').slideToggle(200);
+
+      if($(Product.Class.swatchImage).length) {
+        const image = $(this).next().find('.product-form__swatch-image').attr('src');
+        const textSoldOut =  $(this).next().find('.product-form__swatch-soldout').text();
+        $(Product.Class.swatchImage).attr('src', image).show();
+        $(Product.Class.swatchSoldOut).text(textSoldOut);
+      }
     })
   },
 
