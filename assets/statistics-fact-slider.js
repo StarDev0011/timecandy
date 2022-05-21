73e6e39c5f1a2bb938bfe19cbac7bf26c8534736
chalalala <dtmai1@cmcglobal.vn>
2022-05-21 11:54:18 +0700

    Add spacebetween and next slides

--- assets/statistics-fact-slider.js
+++ assets/statistics-fact-slider.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const swiper = new Swiper("#statistics-fact-slider", {
 		loop: true,
-		slidesPerView: 1,
+		slidesPerView: 1.5,
+		spaceBetween: 48,
 		centeredSlides: true,
 	});
 });
