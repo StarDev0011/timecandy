bd077a21248fed980be27a9ad93298ce551417dd
chalalala <dtmai1@cmcglobal.vn>
2022-05-21 11:52:49 +0700

    Fix slider

---
+++ assets/statistics-fact-slider.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const swiper = new Swiper("#statistics-fact-slider", {
+		loop: true,
+		slidesPerView: 1,
+		centeredSlides: true,
+	});
+});
