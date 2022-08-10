839b159946dd212ed2fe64a4b23fd2ff3aa8b4cb
unknown <tosky0515@gmail.com>
2022-08-10 06:15:14 -0400

    updated for Search animation issue

--- assets/predictive-search.js
+++ assets/predictive-search.js
@@ -25,7 +25,6 @@ class PredictiveSearch extends HTMLElement {
       this.onKeydown.bind(this);
       if ((e.keyCode || e.which) === 27) this.onFocusOut.bind(this)
     });
-    this.animationWorker();
   }
 
   getQuery() {
@@ -250,93 +249,7 @@ class PredictiveSearch extends HTMLElement {
     this.isOpen = false;
   }
 
-  animationWorker() {
-    const string = window.placeholder;
-    var texts = string.split(', ');
-    const input = document.querySelector('input[type="search"]');
-    const animationWorker = function (input, texts) {
-      this.input = input;
-      this.defaultPlaceholder = this.input.getAttribute('placeholder');
-      this.texts = texts;
-      this.curTextNum = 0;
-      this.curPlaceholder = '';
-      this.blinkCounter = 0;
-      this.animationFrameId = 0;
-      this.animationActive = false;
-      this.input.setAttribute('placeholder',this.curPlaceholder);
-
-      this.switch = (timeout) => {
-        this.input.classList.add('imitatefocus');
-        setTimeout(
-          () => {
-            this.input.classList.remove('imitatefocus');
-            if (this.curTextNum == 0)
-              this.input.setAttribute('placeholder',this.defaultPlaceholder);
-            else
-              this.input.setAttribute('placeholder',this.curPlaceholder);
-
-            setTimeout(
-              () => {
-                this.input.setAttribute('placeholder',this.curPlaceholder);
-                if(this.animationActive)
-                  this.animationFrameId = window.requestAnimationFrame(this.animate)},
-              timeout);
-          },
-          timeout);
-      }
-
-      this.animate = () => {
-        if(!this.animationActive) return;
-        let curPlaceholderFullText = this.texts[this.curTextNum];
-        let timeout = 900;
-        if (this.curPlaceholder == curPlaceholderFullText+'|' && this.blinkCounter==3) {
-          this.blinkCounter = 0;
-          this.curTextNum = (this.curTextNum >= this.texts.length-1)? 0 : this.curTextNum+1;
-          this.curPlaceholder = '|';
-          this.switch(3000);
-          return;
-        }
-        else if (this.curPlaceholder == curPlaceholderFullText+'|' && this.blinkCounter<3) {
-          this.curPlaceholder = curPlaceholderFullText;
-          this.blinkCounter++;
-        }
-        else if (this.curPlaceholder == curPlaceholderFullText && this.blinkCounter<3) {
-          this.curPlaceholder = this.curPlaceholder+'|';
-        }
-        else {
-          this.curPlaceholder = curPlaceholderFullText
-            .split('')
-            .slice(0,this.curPlaceholder.length+1)
-            .join('') + '|';
-          timeout = 150;
-        }
-        this.input.setAttribute('placeholder',this.curPlaceholder);
-        setTimeout(
-          () => { if(this.animationActive) this.animationFrameId = window.requestAnimationFrame(this.animate)},
-          timeout);
-      }
-
-      this.stop = () => {
-        this.animationActive = false;
-        window.cancelAnimationFrame(this.animationFrameId);
-      }
-
-      this.start = () => {
-        this.animationActive = true;
-        this.animationFrameId = window.requestAnimationFrame(this.animate);
-        return this;
-      }
-    }
-
-    document.addEventListener("DOMContentLoaded", () => {
-      let aw = new animationWorker(input, texts).start();
-      input.addEventListener("focus", (e) => aw.stop());
-      input.addEventListener("blur", (e) => {
-        aw = new animationWorker(input, texts);
-        if(e.target.value == '') setTimeout( aw.start, 5000);
-      });
-    });
-  }
+  
 }
 
 customElements.define('predictive-search', PredictiveSearch);
